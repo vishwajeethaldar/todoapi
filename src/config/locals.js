05d929913b7a6c302fad0d5d1fc43c7bdaaabba6
vishwajeethaldar <onlyjeet3@gmail.com>
2023-01-23 07:52:41 +0530

    nodemailer with zohomail

--- src/config/locals.js
+++ src/config/locals.js
@@ -5,7 +5,15 @@ dotenv.config()
 let configs = (function (){
     let mongouri = process.env.MONGO_URI;
     let port = process.env.PORT;
-    return {mongouri, port};
+
+    // Zoho Mail Information
+    let Z_Mail = process.env.Z_Mail;
+    let Z_Mail_Password= process.env.Z_Mail_Pass;
+    let Z_Mail_Host= process.env.Z_Mail_Host;
+    let Z_Mail_Port = process.env.Z_Mail_Port;
+    let Z_Sec_Type = process.env.Z_Sec_Type;
+
+    return {mongouri, port,Z_Mail,Z_Mail_Password,Z_Mail_Host,Z_Mail_Port,Z_Sec_Type};
 })()
 
 
